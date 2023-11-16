ec8495736a0f30653eb8f23ca6a816016e3e9fff
MounishaIT <118599104+MounishaIT@users.noreply.github.com>
2023-11-16 17:53:17 +0530

    Update Loginpage.jsx

--- src/components/Footer/Loginpage.jsx
+++ src/components/Footer/Loginpage.jsx
@@ -1,90 +1,74 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './Loginpage.css';
-// import loginImage from './login.jpg'; // Import your login image
 
 function LoginPage() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   
   const navigate = useNavigate();
+
   const handleLogin = async (e) => {
     e.preventDefault();
 
     if (!username || !password) {
-      alert('Please fill in all fields'); 
+      alert('Please fill in all fields');
       return;
     }
 
     try {
-      const response = await fetch('/coachlog', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password }),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        localStorage.setItem('token', data.token);
-   
-        alert('Login Successfull');
-        navigate("/Profile");
+      // Check if the provided username and password match the expected values
+      if (username === 'mounisha' && password === '12345') {
+        // Simulate a successful login
+        localStorage.setItem('token', 'your_generated_token');
+        alert('Login Successful');
+        navigate('/Profile');
       } else {
-        alert('Invalid username or password'); 
+        // Simulate an unsuccessful login
+        alert('Invalid username or password');
       }
     } catch (error) {
       console.error('Error:', error);
-      alert('An error occurred during login.'); 
+      alert('An error occurred during login.');
     }
   };
- 
 
   return (
     <div className='log'>
-    <div className="login-container" >
-      <div className="login-content">
-        <div className="login-form">
-          <h2>Login</h2>
-      
-          <form onSubmit={handleLogin}>
-            <div className="form-group">
-              
-            <input
+      <div className="login-container" >
+        <div className="login-content">
+          <div className="login-form">
+            <h2>Login</h2>
+        
+            <form onSubmit={handleLogin}>
+              <div className="form-group">
+                <input
                   type="text"
                   id="input1"
                   name="input1"
-                
                   required
                   placeholder="Username"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
                 />
-            </div>
-            <div className="form-group">
-            <input
+              </div>
+              <div className="form-group">
+                <input
                   type="password"
                   id="input2"
                   name="input2"
-               
                   required
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                 />
-            </div>
-            <button type="submit">Login</button><br/><br/>
-            Don't have a account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
-          </form>
+              </div>
+              <button type="submit">Login</button><br/><br/>
+              Don't have an account? <Link to='/SignupPage' style={{color:"#007FFF", textDecoration:"none", fontSize: "15px"}}>Create a new account</Link>
+            </form>
+          </div>
         </div>
-        {/* <div className="login-image">
-          <img src={loginImage} alt="Login" />
-         </div>
-         */}
       </div>
-    </div>
     </div>
   );
 }
